321308809aa7b662e90bb447da9334a828a39f2a
NamJuPark <qkrska66@hanyang.ac.kr>
2017-08-12 13:31:45 +0900

    Nam_start

---
+++ app/src/main/java/com/example/namju_flytofit/Show_Exercise_Category.java
@@ -0,0 +1,13 @@
+package com.example.namju_flytofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Show_Exercise_Category extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show__exercise__category);
+    }
+}
